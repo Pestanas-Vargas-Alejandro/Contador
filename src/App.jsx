84413d2fdb08301b9050b4dadec0122de36b6f78
Pestañas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-01 10:07:49 -0600

    Contador incremente

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function App() {
+  //let counter = 0;
+  const [counter, setCounter ] = useState(0)
+  return (
+    <div>
+       <h2>Counter</h2>
+       <hr />
+       <h1>{counter}</h1>
+       <button
+       onClick={() => {
+        setCounter (counter + 1);
+        console.log(counter)
+      }}
+       type="button" 
+       className="btn btn-primary">
+        + 1
+        </button>
+    </div>
+  )
+}
+
+export default App
