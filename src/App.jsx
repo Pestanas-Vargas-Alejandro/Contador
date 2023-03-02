ab849027158d4165acaba48043e550dd0486e325
Pestañas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-02 09:15:03 -0600

    Contador decremente y reinicie

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,45 @@ import { useState } from "react";
 
 function App() {
   //let counter = 0;
-  const [counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0)
   return (
-    <div>
-       <h2>Counter</h2>
-       <hr />
-       <h1>{counter}</h1>
-       <button
-       onClick={() => {
-        setCounter (counter + 1);
-        console.log(counter)
-      }}
-       type="button" 
-       className="btn btn-primary">
+    <div className="container mt-5">
+      <h2>Counter</h2>
+      <hr />
+      <h1>{counter}</h1>
+      <h4>Counter</h4>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          console.log(counter)
+        }}
+        type="button"
+        className="btn btn-success">
         + 1
-        </button>
+      </button>
+      <h4>Discounter</h4>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+          console.log(counter)
+        }}
+        type="button"
+        className="btn btn-danger">
+        - 1
+      </button>
+      <h4>Restart</h4>
+      <button
+        onClick={() => {
+          setCounter(0);
+          console.log(counter)
+        }}
+        type="button"
+        className="btn btn-warning">
+        0
+      </button>
     </div>
+
+
   )
 }
 
